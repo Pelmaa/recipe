937d4033598b29aa4c6067b6ab45444b618442f7
Pema Wangchuk <pewangk703@gmail.com>
2025-06-23 22:31:52 +0600

    added recipe

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const connectMongoDB = require("./db/mongo.db");
 const authRoutes = require("./routes/auth.route");
 const foodRoutes = require("./routes/food.routes");
-const { forntendUrl } = require("./config");
+const { frontendUrl } = require("./config");
 
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 connectMongoDB();
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: forntendUrl,
+    origin: frontendUrl,
     credentials: true,
   })
 );
@@ -22,10 +22,8 @@ app.use(
 app.use("/auth", authRoutes);
 app.use("/food", foodRoutes);
 
-app.get("/health", (_req, res) => res.send("Server is healthy, its connected"));
-app.get("/api/test", (_req, res) =>
-  res.json({ message: "CORS from 6000 works!" })
-);
+app.get("/", (_req, res) => res.send("Server is healthy, its connected"));
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
