98918821d81d4ff2f871c078379821ef352f413e
Pema Wangchuk <pewangk703@gmail.com>
2025-06-23 22:33:34 +0600

    added recipe

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -5,5 +5,5 @@ dotenv.config();
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
-  forntendUrl: process.env.FRONTEND_URL,
+  frontendUrl: process.env.FRONTEND_URL,
 };
